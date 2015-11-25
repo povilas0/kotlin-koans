cbad0446bf417525049051f57793eb27933a48e6
Povilas Dauksas <povilas.d@gmail.com>
2015-11-25 23:12:35 +0000

    Made 26_InRange test to pass

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,14 +1,14 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) {
+    operator fun compareTo(other: MyDate) = when {
+        this.year != other.year -> this.year - other.year
+        this.month != other.month -> this.month - other.month
+        else -> this.dayOfMonth - other.dayOfMonth
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
-
-operator fun MyDate.compareTo(other: MyDate) = when {
-    this.year != other.year -> this.year - other.year
-    this.month != other.month -> this.month - other.month
-    else -> this.dayOfMonth - other.dayOfMonth
-}
 
 enum class TimeInterval {
     DAY,
@@ -16,4 +16,6 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+    operator fun contains(date: MyDate) = date >= start && date <= endInclusive
+}
