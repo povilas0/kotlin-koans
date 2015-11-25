bc543fb9d006d9adcaf6eb3f3560fbd7b4e9f031
Povilas Dauksas <povilas.d@gmail.com>
2015-11-25 23:56:19 +0000

    Made 30_Multi_Assignment test to pass

--- src/iii_conventions/_30_DestructuringDeclarations.kt
+++ src/iii_conventions/_30_DestructuringDeclarations.kt
@@ -10,12 +10,17 @@ fun todoTask30(): Nothing = TODO(
     documentation = doc30()
 )
 
-class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) {
+    operator fun component1() = year
+
+    operator fun component2() = month
+
+    operator fun component3() = dayOfMonth
+}
 
 fun isLeapDay(date: MyDate): Boolean {
-    todoTask30()
-//    val (year, month, dayOfMonth) = date
+    val (year, month, dayOfMonth) = date
 //
 //    // 29 February of a leap year
-//    return year % 4 == 0 && month == 2 && dayOfMonth == 29
+    return year % 4 == 0 && month == 2 && dayOfMonth == 29
 }
