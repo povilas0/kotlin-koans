3e7f081149f2d0664cac252af3aa7cb1b00911fc
Povilas Dauksas <povilas.d@gmail.com>
2015-12-15 21:15:35 +0000

    Made 32_Properties test to pass

--- src/iv_properties/_32_Properties_.kt
+++ src/iv_properties/_32_Properties_.kt
@@ -4,7 +4,11 @@ import util.*
 
 class PropertyExample() {
     var counter = 0
-    var propertyWithCounter: Int? = todoTask32()
+    var propertyWithCounter: Int? = 0
+        set(value) {
+            field = value
+            counter++
+        }
 }
 
 fun todoTask32(): Nothing = TODO(
